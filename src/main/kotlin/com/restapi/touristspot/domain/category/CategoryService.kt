3d25f4b1c730ea218e3f21b43062fa29f6f66034
Demysdcl <demysdcl@gmail.com>
2020-05-31 20:41:35 -0300

    Not save spot with noexistent category

--- src/main/kotlin/com/restapi/touristspot/domain/category/CategoryService.kt
+++ src/main/kotlin/com/restapi/touristspot/domain/category/CategoryService.kt
@@ -1,13 +1,14 @@
 package com.restapi.touristspot.domain.category
 
 import com.restapi.touristspot.exception.ObjectAlreadyExistsException
+import com.restapi.touristspot.exception.ObjectNotFoundException
 import org.springframework.stereotype.Service
 
 @Service
 class CategoryService(private val categoryRepository: CategoryRepository) {
 
     fun find(name: String) = categoryRepository.findById(name)
-            .orElse(categoryRepository.save(Category(name)))
+            .orElseThrow { ObjectNotFoundException("Category not found") }
 
     fun save(category: String): Category = categoryRepository.findById(category)
             .let {
